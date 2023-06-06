bba5ba2ac1e80d5f1962f6da70f1733927e41a17
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-06-06 09:53:32 -0700

    shows the refactor happening within my fetch call, thank god

--- src/Component/App/App.tsx
+++ src/Component/App/App.tsx
@@ -14,6 +14,8 @@ export type SongDeets = {
   artistId: number
   artistName: string
   rank: number
+  rating?: number
+  favorite?: boolean
 }
 
 const App: React.FC = () => {
@@ -22,78 +24,73 @@ const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      
-      const pierreData: SongDeets[] | undefined = await fetchPierreBourneData()
-      const radioHeadData: SongDeets[] | undefined  = await fetchRadioHeadData()
-      const frankData: SongDeets[] | undefined  = await fetchFrankOceanData()
-      const asapData: SongDeets[] | null = await fetchAsapRockyData()
-      const lanaData: SongDeets[] | undefined = await fetchLanaDelReyData()
-      const deftonesData: SongDeets[] | undefined = await fetchDeftonesData()
-      const destroyData: SongDeets[] | undefined = await fetchDestroyLonelyData()
-      const cartiData: SongDeets[] | undefined = await fetchPlayboiCarti()
-      const cureData: SongDeets[] | undefined = await fetchTheCureData()
-      const kendrickData: SongDeets[] | undefined = await fetchKendrickLamarData()
-      // const combinedData = [...asapData,...lanaData, ...frankData,...radioHeadData, ...pierreData, ...deftonesData,...destroyData,...cartiData, ...cureData, ...kendrickData]
-      if (pierreData) {
-        setSongs(pierreData);
-      } else {
-        setSongs([]);
-      }
-      if (radioHeadData) {
-        setSongs(prevSongs => [...prevSongs, ...radioHeadData]);
-      }
-      if (frankData) {
-        setSongs(prevSongs => [...prevSongs, ...frankData]);
-      }
-      if (lanaData) {
-        setSongs(prevSongs => [...prevSongs, ...lanaData]);
-      }
-      if (deftonesData) {
-        setSongs(prevSongs => [...prevSongs, ...deftonesData]);
-      }
-      if (destroyData) {
-        setSongs(prevSongs => [...prevSongs, ...destroyData]);
-      }
-      if (cartiData) {
-        setSongs(prevSongs => [...prevSongs, ...cartiData]);
-      }
-      if (cureData) {
-        setSongs(prevSongs => [...prevSongs, ...cureData]);
-      }
-      if (cureData) {
-        setSongs(prevSongs => [...prevSongs, ...cureData]);
-      }
-      if (asapData) {
-        setSongs(prevSongs => [...prevSongs, ...asapData]);
-      }
-      if (kendrickData) {
-        setSongs(prevSongs => [...prevSongs,...kendrickData]);
-      }
+  const chooseSong = (song: SongDeets) => {
+    const newFavState = [...favorites, song];
+    setFavorites(newFavState);
+  }
+
+  const removeFavorite = (song: SongDeets) => {
+    const newFavState = favorites.filter(fav => fav.id!== song.id);
+    setFavorites(newFavState);
+  }
+  const fetchData = async () => {
+    const fetchFunctions = [
+      fetchPierreBourneData(),
+      fetchRadioHeadData(),
+      fetchFrankOceanData(),
+      fetchAsapRockyData(),
+      fetchLanaDelReyData(),
+      fetchDeftonesData(),
+      fetchDestroyLonelyData(),
+      fetchPlayboiCarti(),
+      fetchTheCureData(),
+      fetchKendrickLamarData()
+    ];
+  
+    try {
+      const fetchedData = await Promise.all(fetchFunctions);
+      const combinedData = fetchedData.flat() as SongDeets[]
+      setSongs(combinedData);
+      setLoading(false);
+    } catch (error) {
+      setError(error as Error | null);
       setLoading(false);
     }
+  };
+  
+  useEffect(() => {
     fetchData();
   }, []);
 
+  
+
   return (
-    <div className="App">
-     <h1> Song Rate</h1>
+    <section className="App">
      <Header/>
        {loading && <h2>Loading...</h2>}
        {error && <Error />}
      <Switch>
-       <Route exact path="/" render={() => (
-         <Main data={songs}/>
+       <Route 
+        exact path="/" 
+        render={() => (
+         <Main 
+          data={songs}
+          chooseSong={chooseSong}
+          removeFavorite={removeFavorite}/>
          )}
        />
-      <Route exact path="/favorites" render={() => (
-        <Main data={favorites}/>
+      <Route 
+        exact path="/favorites" 
+        render={() => (
+        <Main 
+          data={favorites}
+          chooseSong={chooseSong}
+          removeFavorite={removeFavorite}/>
         )} 
       />
       <Route path="*" render={() => <Error />}/>
      </Switch>
-    </div>
+    </section>
   );
 }
 
--- src/Component/Card/Song.tsx
+++ src/Component/Card/Song.tsx
@@ -1,13 +1,33 @@
 import React, {useState, useEffect, FC} from "react";
 import { SongDeets } from "../App/App";
+
 interface SongProps {
     song: SongDeets
+    chooseSong: (song: SongDeets) => void;
+    removeFavorite: (song: SongDeets) => void;
 }
-export const Song: FC<SongProps> = (props: SongProps) => {
+export const Song: React.FC<SongProps> = (props: SongProps) => {
+    const {song, chooseSong, removeFavorite} = props
+    const [favorite, setFavorite] = useState(false);
+
+   const handleFavorite = () => {
+    if (!favorite) {
+        setFavorite(true);
+        chooseSong(song);
+        } else {
+        setFavorite(false);
+        removeFavorite(song);
+   }
+   }
     return (
         <div>
-            <img src={props.song.albumCover} alt={props.song.albumTitle} />
-            <h3>{props.song.albumTitle}</h3>
+            <img src={song.albumCover} alt={song.albumTitle} />
+            <h3>{song.albumTitle}</h3>
+            <button 
+            name={song.title}
+            onClick={handleFavorite}>
+                {favorite ? "Unfavorite" : "Favorite"}
+            </button>
         </div>
     )
 }
