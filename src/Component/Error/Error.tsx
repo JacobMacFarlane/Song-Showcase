7799586f4019959e006cbd79dd545bfd883a282c
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-06-08 14:46:29 -0700

    shows the abd routes testeddddd

--- src/Component/Error/Error.tsx
+++ src/Component/Error/Error.tsx
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 
-export const Error = () => {
+interface ErrorProps {
+    message?: string
+}
+
+
+export const Error: React.FC<ErrorProps> = ({ message }) => {
+    const shownText = message ? <p>{message}</p> : <Link to="/"> <h2 className="go-back">Click here and lets go back home pal!</h2></Link>
+    
     return (
         <div>
-            <p>
-                Hello Error
-            </p>
+            <h2>Oops!!!!</h2>
+            {shownText}
         </div>
+        
     )
 }
